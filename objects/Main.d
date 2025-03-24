55f302cc19b14d01f5e84c0f40fa176be6ee4899
pjlab <qinzhao2003@gmail.com>
2025-03-24 21:03:42 +0800

    first init with lcm class

---
+++ objects/Main.d
@@ -0,0 +1,4 @@
+objects/Main.o: Main.cpp ClientLogging.hpp SDKClient.hpp glove.hpp \
+ ClientPlatformSpecific.hpp ClientPlatformSpecificTypes.hpp \
+ ManusSDK/include/ManusSDK.h ManusSDK/include/ManusSDKTypes.h \
+ ManusSDK/include/ManusSDKTypeInitializers.h
